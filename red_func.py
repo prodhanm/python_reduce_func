aeee1b7ec42bf2af727c783e8c67db6ad2616aae
prodhanm <mustafizur.prodhan@gmail.com>
2023-08-25 07:04:39 -0400

    added exception handling to the code.

--- red_func.py
+++ red_func.py
@@ -3,8 +3,11 @@ from functools import reduce
 nums = [num for num in range(1,11)]
 
 def main():
-    product = reduce(lambda x,y: x*y, nums)
-    return product
+    try:
+        product = reduce(lambda x,y: x*y, nums)
+        return product
+    except TypeError as err:
+        print(f"{err}: needs values added to the list.")
 
 if __name__ == "__main__":
     print(main())
