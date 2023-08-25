c8560cba448de29d0f93abcdb89f5443252737ca
prodhanm <mustafizur.prodhan@gmail.com>
2023-08-25 06:49:57 -0400

    used reduce() to iterate over a list to get the product of multiplication.

--- red_func.py
+++ red_func.py
@@ -1,8 +1,11 @@
 from functools import reduce
 
+nums = [num for num in range(1,11)]
+
 def main():
-    pass
+    product = reduce(lambda x,y: x*y, nums)
+    return product
 
 if __name__ == "__main__":
-    main()
+    print(main())
 
